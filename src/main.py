0c3be5c00836ebac84abdbc68e88f58f6e719da1
Ubuntu <ubuntu@ip-172-31-20-242.ec2.internal>
2020-09-20 13:34:29 +0000

    Initial API requests working as intended

--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
 import requests
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+DEEP_API_KEY = os.getenv("DEEP_API_KEY")
+
+query = "from:mkbhd"
+
+tweet_fields = "tweet.fields=author_id"
+
+url = f"https://api.twitter.com/2/tweets/search/recent?query={query}&{tweet_fields}"
+
+headers = {"Authorization": "Bearer {}".format(BEARER_TOKEN)}
+
+response = requests.get(url, headers=headers)
+latest_tweet = json.loads(response.text)['data'][0]['text']
+
+print(latest_tweet)
 
 r = requests.post(
     "https://api.deepai.org/api/sentiment-analysis",
     data={
-        'text': 'This is awesome!',
+        'text': f'{latest_tweet}',
     },
-    headers={'api-key': '644f22d1-b49f-44ab-aca2-f849fdf7d411'}
+    headers={'api-key': DEEP_API_KEY}
 )
 print(r.json())
