9e70f3a93df7ec089323cd0120a3c88d1b83a5eb
Samitha Padukkage Don <samitha.312@gmail.com>
2020-09-24 12:06:48 +1000

    Twitter class separation and docs started

--- Twitter.py
+++ Twitter.py
@@ -3,4 +3,23 @@ from dotenv import load_dotenv
 import os
 import json
 
-def 
+load_dotenv()
+
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+
+class Twitter():
+    @classmethod
+    def get_tweet(cls, twitter_id):
+        try:
+            query = f"from:{twitter_id}"
+            tweet_fields = "tweet.fields=author_id"
+            url = f"https://api.twitter.com/2/tweets/search/recent?query={query}&{tweet_fields}"
+            # headers = {"Authorization": "Bearer {}".format(BEARER_TOKEN)}
+            headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
+
+            response = requests.get(url, headers=headers)
+            latest_tweet = json.loads(response.text)['data'][0]['text']
+
+            return latest_tweet
+        except Exception:
+            return None
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import os
 import json
 
+from Twitter import Twitter
+
 load_dotenv()
 
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
@@ -10,16 +12,9 @@ DEEP_API_KEY = os.getenv("DEEP_API_KEY")
 
 twitter_user = input("Enter twitter username: ")
 
-query = f"from:{twitter_user}"
+twitter = Twitter()
 
-tweet_fields = "tweet.fields=author_id"
-
-url = f"https://api.twitter.com/2/tweets/search/recent?query={query}&{tweet_fields}"
-
-headers = {"Authorization": "Bearer {}".format(BEARER_TOKEN)}
-
-response = requests.get(url, headers=headers)
-latest_tweet = json.loads(response.text)['data'][0]['text']
+latest_tweet = twitter.get_tweet(twitter_user)
 
 print(latest_tweet)
 
