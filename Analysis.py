d714cdba32ae6411772a021b7f2675ca42ac4533
Samitha Padukkage Don <samitha.312+1@gmail.com>
2020-09-25 12:42:44 +1000

    Created Analysis class and separated from main

---
+++ Analysis.py
@@ -0,0 +1,43 @@
+import requests
+from dotenv import load_dotenv  # type: ignore
+import os
+
+load_dotenv()
+
+DEEP_API_KEY = os.getenv("DEEP_API_KEY")
+
+class Analysis():
+    data = []
+
+    @classmethod
+    def analyse_text(cls, text):
+        r = requests.post(
+            "https://api.deepai.org/api/sentiment-analysis",
+            data={
+                'text': text,
+            },
+            headers={
+                'api-key': DEEP_API_KEY
+            }
+        )
+
+        sentiments = r.json()['output']
+
+        total = len(sentiments)
+
+        neutral = sentiments.count('Neutral')
+        positive = sentiments.count('Positive')
+        negative = sentiments.count('Negative')
+
+        print(f'''
+        Sentiment Analysis
+        ------------------
+
+        Total phrases analysed: {total}
+
+        {positive} positive phrases, {(positive / total * 100):.2f}%
+        {neutral} neutral phrases, {(neutral / total * 100):.2f}%
+        {negative} negative phrases, {(negative / total * 100):.2f}%
+
+
+        ''')
--- main.py
+++ main.py
@@ -1,47 +1,12 @@
-import requests
-from dotenv import load_dotenv  # type: ignore
-import os
-
 from Twitter import Twitter
+from Analysis import Analysis
 
-load_dotenv()
 twitter = Twitter()
-
-DEEP_API_KEY = os.getenv("DEEP_API_KEY")
+analysis = Analysis()
 
 twitter_user = input("Enter twitter username or search query: ")
 
 latest_tweets = twitter.get_tweets(twitter_user)
 
 if latest_tweets:
-
-    r = requests.post(
-        "https://api.deepai.org/api/sentiment-analysis",
-        data={
-            'text': latest_tweets,
-        },
-        headers={
-            'api-key': DEEP_API_KEY
-        }
-    )
-
-    sentiments = r.json()['output']
-
-    total = len(sentiments)
-
-    neutral = sentiments.count('Neutral')
-    positive = sentiments.count('Positive')
-    negative = sentiments.count('Negative')
-
-    print(f'''
-    Sentiment Analysis
-    ------------------
-
-    Total phrases analysed: {total}
-
-    {positive} positive phrases, {(positive / total * 100):.2f}%
-    {neutral} neutral phrases, {(neutral / total * 100):.2f}%
-    {negative} negative phrases, {(negative / total * 100):.2f}%
-
-
-    ''')
+    analysis.analyse_text(latest_tweets)
