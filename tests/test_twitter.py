c24db08956912503b9d5ef406007a3ea76d6096f
Samitha Padukkage Don <samitha.312+1@gmail.com>
2020-09-27 19:38:44 +1000

    Added more Twitter tests

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -3,13 +3,23 @@ import Twitter
 from Twitter import Twitter
 
 class TestTwitterMethods(unittest.TestCase):
-    def test_tests_running(self):
-        self.assertTrue(True)
 
     def test_get_tweets_user(self):
         twitter = Twitter()
         user = '@twitter'
         result = twitter.get_tweets(user)
+        self.assertIsInstance(result, list)
+
+    def test_get_tweets_search(self):
+        twitter = Twitter()
+        search_term = 'news'
+        result = twitter.get_tweets(search_term)
+        self.assertIsInstance(result, list)
+
+    def test_get_tweets_hashtag(self):
+        twitter = Twitter()
+        hashtag = '#news'
+        result = twitter.get_tweets(hashtag)
         self.assertIsInstance(result, list)
 
     def test_parse_tweets(self):
