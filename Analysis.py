08f66b47784ddf0816885c37ca816cfeb02d9eef
Samitha Padukkage Don <samitha.312+1@gmail.com>
2020-10-01 13:44:40 +1000

    Added docstrings

--- Analysis.py
+++ Analysis.py
@@ -14,6 +14,15 @@ class Analysis():
 
     @classmethod
     def analyse_text(cls, text: str) -> List[str]:
+        """Send a string to the Deep AI API to analyse and return the results
+
+        Args:
+            text (str): A string of phrases to be analysed for their sentiment
+
+        Returns:
+            List[str]: A list of strings the gives the sentiment of each
+            analysed phrase
+        """
         print("\nSending data to Deep AI for analysis...\n")
         try:
             response = requests.post(
@@ -34,6 +43,15 @@ class Analysis():
 
     @classmethod
     def collate_data(cls, sentiments: List[str]) -> dict:
+        """Take the list of strings returned from the Deep AI
+        API and collate all the data in a dictionary
+
+        Args:
+            sentiments (List[str]): [description]
+
+        Returns:
+            dict: [description]
+        """
 
         total = len(sentiments)
 
@@ -56,6 +74,13 @@ class Analysis():
 
     @classmethod
     def view_report(cls, data: dict, saved: bool = False) -> None:
+        """Print out a report of the sentiment analysis for the user to view
+
+        Args:
+            data (dict): The data to be used to generate the report
+            saved (bool, optional): Whether the report is
+            already saved or not. Defaults to False.
+        """
 
         print(f'''
         Sentiment Analysis
@@ -85,7 +110,15 @@ class Analysis():
             user_option = input("Press Enter to return to main menu.")
 
     @classmethod
-    def read_file(cls, file):
+    def read_file(cls, file: str) -> str:
+        """[summary]
+
+        Args:
+            file (str): The path and/or filename for the file to be read
+
+        Returns:
+            str: Contents of the file
+        """
         try:
             with open(file) as f:
                 data = f.read()
@@ -96,7 +129,15 @@ class Analysis():
             return ''
 
     @classmethod
-    def save_file(cls, report):
+    def save_file(cls, report: dict) -> dict:
+        """Add new report to already saved reports and save to file
+
+        Args:
+            report (dict): The newly generated sentiment analysis report
+
+        Returns:
+            dict: The saved report
+        """
         name = input("\nWhat would you like to name this report?")
 
         saved = {
@@ -115,6 +156,8 @@ class Analysis():
 
     @classmethod
     def load_saved(cls):
+        """Loads previously saved reports, run on app startup
+        """
         try:
             json_data = cls.read_file('saved_reports.json')
             cls.data = json.loads(json_data)
@@ -126,6 +169,9 @@ class Analysis():
 
     @classmethod
     def view_saved(cls):
+        """Shows a list of the names of saved reports for the user to select,
+        the selected report is then generated to be viewed by the user
+        """
         print("")
         for index, report in enumerate(cls.data):
             print(f"{index+1}. {report['name']}")
--- Twitter.py
+++ Twitter.py
@@ -11,6 +11,14 @@ BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 class Twitter():
     @classmethod
     def get_tweets(cls, search_term: str) -> list:
+        """Request tweets from Twitter API using search term provided by user.
+
+        Args:
+            search_term (str): String input is the search term provided by user
+
+        Returns:
+            list: A list of dictionaries of each tweet provided by Twitter API
+        """
         try:
             if search_term[0] == '@':
                 query = f"from:{search_term[1:]}"
@@ -37,6 +45,18 @@ class Twitter():
 
     @classmethod
     def parse_tweets(cls, tweets: list) -> str:
+        """Takes each tweet from a list of tweets retrieved from
+        get_tweets method and parses them into a single string.
+        Just the text content from each tweet is taken, and any
+        links are removed.
+
+        Args:
+            tweets (list): List of tweets retrieved from get_tweets method
+
+        Returns:
+            str: The text of all the strings of each tweet
+            joined with '. ' and all links removed
+        """
         tweet_text = ''
 
         for tweet in tweets:
