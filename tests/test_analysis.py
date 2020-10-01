4b8d16708380b81380d5948be729e70174206c50
Samitha Padukkage Don <samitha.312+1@gmail.com>
2020-10-02 09:46:16 +1000

    Add first Analysis test

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -4,4 +4,13 @@ from Analysis import Analysis
 
 class TestAnalysisMethods(unittest.TestCase):
 
-    pass
+    def test_analyse_text(self):
+        """Test if retrieving specific user tweets returns list correctly.
+        This test will return an empty list and should still pass if 
+        there is no Twitter Bearer Token available.
+        """
+        analysis = Analysis()
+        test_input = "This is some text to analyse.\
+             If a Deep AI API Key is not available to use it will return an empty list."
+        result = analysis.analyse_text(test_input)
+        self.assertIsInstance(result, list)
